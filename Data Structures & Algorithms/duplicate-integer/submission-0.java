7e32e7c133d45a734b84f69dbb468a191b88dca8
tirthchoksi72 <tirthchoksi72@gmail.com>
2026-08-26 21:35:09 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i<nums.length ; i++){
+            map.put(nums[i],(map.getOrDefault(nums[i],0)+1));
+        }
+        for (Integer i : map.values()){
+            if (i > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
